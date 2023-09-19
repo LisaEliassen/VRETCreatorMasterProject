bf338a079ea955906aa44e44b29e9356b96df4eb
Lisa Maria Eliassen <77976790+LisaEliassen@users.noreply.github.com>
2023-09-19 20:53:52 +0200

    Added some sliders

---
+++ PhobiaFramework/Assets/Code/SliderController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SliderController : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI sliderText = null;
+    [SerializeField] private float maxSliderValue = 100.0f;
+
+    public void SliderChange(float value) 
+    {
+        float localValue = value * maxSliderValue;
+        sliderText.text = localValue.ToString("0");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
