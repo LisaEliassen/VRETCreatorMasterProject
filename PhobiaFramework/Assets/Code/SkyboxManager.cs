3009f4219a017dce75894c872adbb57b45dec4cc
Princess Oly <olesya2011@hotmail.no>
2023-10-18 13:39:14 +0200

    Implemented 360 video displayer

--- PhobiaFramework/Assets/Code/SkyboxManager.cs
+++ PhobiaFramework/Assets/Code/SkyboxManager.cs
@@ -1,30 +1,66 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
+using UnityEngine.Video;
 
 public class SkyboxManager : MonoBehaviour
 {
     public Material skyboxMaterial;
-    public Button importButton;
+    public Button importImageButton;
+    public Button importVideoButton;
+    public GameObject skyboxObject; // Reference to your Skybox GameObject
+    public VideoPlayer videoPlayer;
+    public RenderTexture renderTexture;
 
     private void Start()
     {
-        importButton.onClick.AddListener(ImportSkybox);
+        importImageButton.onClick.AddListener(ImportImage);
+        importVideoButton.onClick.AddListener(ImportVideo);
     }
 
-    private void ImportSkybox()
+    private void ImportImage()
     {
-        string path = UnityEditor.EditorUtility.OpenFilePanel("Select 360 Image/Video", "", "jpg, png, mp4, mov");
+        string path = UnityEditor.EditorUtility.OpenFilePanel("Select 360 Image", "", "jpg, png");
         if (path.Length != 0)
         {
-            // Load the image or video from the selected file
             byte[] fileData = File.ReadAllBytes(path);
             Texture2D equirectangularImage = new Texture2D(2, 2);
             equirectangularImage.LoadImage(fileData);
 
             // Set the loaded texture as the Skybox texture
             skyboxMaterial.SetTexture("_MainTex", equirectangularImage);
+
+            // Activate the Skybox GameObject
+            skyboxObject.SetActive(true);
+
+            // Deactivate the VideoPlayer if it's active
+            videoPlayer.Stop();
+            videoPlayer.targetTexture = null;
+        }
+    }
+
+    private void ImportVideo()
+    {
+        string path = UnityEditor.EditorUtility.OpenFilePanel("Select Video", "", "mp4, mov");
+        if (path.Length != 0)
+        {
+            // Set the video source and render texture
+            videoPlayer.source = VideoSource.Url;
+            videoPlayer.url = path;
+            videoPlayer.targetTexture = renderTexture;
+
+            // Set the Render Texture as the Skybox material
+            skyboxMaterial.SetTexture("_MainTex", renderTexture);
+
+            // Activate the Skybox GameObject
+            skyboxObject.SetActive(true);
+
+            // Play the video
+            videoPlayer.Play();
         }
     }
 }
+
+
+
 
