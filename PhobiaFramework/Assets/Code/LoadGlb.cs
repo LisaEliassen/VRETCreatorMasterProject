93763283375416deed08c2da8c45778722d7af00
Princess Oly <olesya2011@hotmail.no>
2023-11-07 15:31:33 +0100

    Dragging trigger object implemented

--- PhobiaFramework/Assets/Code/LoadGlb.cs
+++ PhobiaFramework/Assets/Code/LoadGlb.cs
@@ -20,6 +20,8 @@ public class LoadGlb : MonoBehaviour
     Vector3 position;
     public GameObject posObject;
     string triggerName;
+    private Vector3 offset;
+    private bool isDragging = false;
 
     // Start is called before the first frame update
     void Start()
@@ -66,6 +68,15 @@ public class LoadGlb : MonoBehaviour
 
         //LoadGlbFile(loadedModel, "gs://vr-framework-95ccc.appspot.com/models/crowAnimated.glb", "crowAnimated");
         trigger = new GameObject("Trigger");
+
+        // Add a Box Collider to the GameObject
+        BoxCollider boxCollider = trigger.AddComponent<BoxCollider>();
+
+        // You can also set various properties of the Box Collider
+        boxCollider.isTrigger = false; // Set to true if you want it to be a trigger
+        boxCollider.size = new Vector3(1.0f, 1.0f, 1.0f); // Set the size of the collider
+
+
         LoadGlbFile(trigger, path, modelName);
     }
 
@@ -140,6 +151,46 @@ public class LoadGlb : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
 
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject == trigger)
+                {
+                    // Select the object
+                    isDragging = true;
+                    offset = trigger.transform.position - hit.point;
+                }
+            }
+        }
+
+        if (isDragging && Input.GetMouseButton(0))
+        {
+            Ray ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                // Move the selected object to the mouse position on the plane
+                Vector3 newPosition = hit.point + offset;
+                newPosition.y = trigger.transform.position.y;
+                trigger.transform.position = newPosition;
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0) && isDragging)
+        {
+            // Deselect the object
+            isDragging = false;
+        }
     }
+
+    public void SetSelectedObject(GameObject obj)
+    {
+        trigger = obj;
+    }
+
 }
