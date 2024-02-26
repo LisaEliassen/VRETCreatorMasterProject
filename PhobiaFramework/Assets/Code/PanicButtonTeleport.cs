182ea8a1049cf3a78fde0bab6f9c7f5eaaab84e1
Princess Oly <olesya2011@hotmail.no>
2024-02-26 16:20:45 +0100

    PanicButton functionality with teleportation implemented

---
+++ PhobiaFramework/Assets/Code/PanicButtonTeleport.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Specialized;
+
+public class PanicButtonTeleport : MonoBehaviour
+{
+    public Button stopButton;
+    public GameObject xrRig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stopButton.onClick.AddListener(TeleportPlayer);
+    }
+
+    private void TeleportPlayer()
+    {
+        // Vector is the position which the person will be teleported to
+        xrRig.transform.position = new Vector3(0, 0, 0);
+    }
+}
