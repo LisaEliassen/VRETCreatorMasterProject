054656b01ee061db3e42d776db61001a1f359b31
Princess Oly <olesya2011@hotmail.no>
2024-02-27 16:06:01 +0100

    Panic button teleportation update

--- PhobiaFramework/Assets/Code/PanicButtonTeleport.cs
+++ PhobiaFramework/Assets/Code/PanicButtonTeleport.cs
@@ -8,6 +8,7 @@ public class PanicButtonTeleport : MonoBehaviour
 {
     public Button stopButton;
     public GameObject xrRig;
+    public GameObject xrRigCamera;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +19,7 @@ public class PanicButtonTeleport : MonoBehaviour
     private void TeleportPlayer()
     {
         // Vector is the position which the person will be teleported to
-        xrRig.transform.position = new Vector3(0, 0.89f, 2.93f);
+        xrRig.transform.position = new Vector3(0, 0, 12.37f);
+        xrRigCamera.transform.rotation = Quaternion.Euler(0, 0, 0);
     }
 }
