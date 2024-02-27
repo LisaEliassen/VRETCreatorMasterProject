424406fc6a71eebb5e680fd0a8771004e8305c3d
Princess Oly <olesya2011@hotmail.no>
2024-02-27 17:36:34 +0100

    Hiding waiting room implemented

---
+++ PhobiaFramework/Assets/Code/HideWaitingRoom.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class HideWaitingRoom : MonoBehaviour
+{
+    public GameObject waitingRoom;
+    public GameObject xrRig;
+    public GameObject door1;
+    public GameObject door2;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(door2.transform.rotation.eulerAngles);
+        // Check if the XR rig is past Z value 5 and the door is closed
+        if (xrRig.transform.position.z < 5f && IsDoorClosed())
+        {
+            Debug.Log("Door is closed");
+            // Hide the waiting room
+            waitingRoom.SetActive(false);
+        }
+        else
+        {
+            // Show the waiting room
+            waitingRoom.SetActive(true);
+        }
+    }
+
+    // Method to check if the door is closed
+    bool IsDoorClosed()
+    {
+        if (door1.transform.rotation.eulerAngles.y <= 185 && door2.transform.rotation.eulerAngles.y <= 5)
+        {
+            return true;
+        }
+        else { 
+            return false; 
+        }
+    }
+}
+
