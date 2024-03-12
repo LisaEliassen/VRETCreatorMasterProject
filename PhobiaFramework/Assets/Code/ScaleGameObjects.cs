627e780dbcd0a968c7298b43a75d7527a4f3fd82
Princess Oly <olesya2011@hotmail.no>
2024-03-12 18:50:47 +0100

    Some comments

--- PhobiaFramework/Assets/Code/ScaleGameObjects.cs
+++ PhobiaFramework/Assets/Code/ScaleGameObjects.cs
@@ -10,7 +10,12 @@ public class ScaleGameObjects : MonoBehaviour
     public GameObject sphere;
     public GameObject exposureScene;
     public GameObject waitingRoom;
-    public GameObject doorWay;
+    public GameObject doors;
+
+    public GameObject door1;
+    public GameObject door2;
+    private HingeJoint hingeJoint1;
+    private HingeJoint hingeJoint2;
 
     public Slider sphereSlider;
     public TMP_InputField sizeInputSphere;
@@ -19,6 +24,10 @@ public class ScaleGameObjects : MonoBehaviour
 
     private void Start()
     {
+        //hingeJoint1 = door1.GetComponent<HingeJoint>();
+        //hingeJoint2 = door2.GetComponent<HingeJoint>();
+
+
         // Add listeners to detect changes in the input field values
         sizeInputSphere.onValueChanged.AddListener(delegate { UpdateSphereFromInputField(); });
         sizeInputPlatform.onValueChanged.AddListener(delegate { UpdatePlatformFromInputField(); });
@@ -35,9 +44,40 @@ public class ScaleGameObjects : MonoBehaviour
 
     private void Update()
     {
+        /*
+        hingeJoint1 = door1.GetComponent<HingeJoint>();
+        hingeJoint2 = door2.GetComponent<HingeJoint>();
         // Call the method to update the scale of the objects
-        UpdateSphereScale();
+        
+        Destroy(hingeJoint1);
+        Destroy(hingeJoint2);
         // implement here to disable the hinge joint of the doors and activate it after scaling is finished to fix the issue?
+        print("Before");
+        print(doors.transform.position);
+        print("");
+        
+        print("After");
+        print(doors.transform.position);
+        door1.AddComponent<HingeJoint>();
+        door2.AddComponent<HingeJoint>();
+
+        hingeJoint1 = door1.AddComponent<HingeJoint>();
+        hingeJoint2 = door2.AddComponent<HingeJoint>();
+
+        // Set the limits
+        JointLimits limits = hingeJoint1.limits;
+        limits.min = 0f; // Minimum angle
+        limits.max = 90f; // Maximum angle
+        hingeJoint1.limits = limits;
+        hingeJoint2.limits = limits;
+
+        // Set connected anchor
+        Vector3 anchor1 = new Vector3(1.030685f, 1.004f, -4.9399f);
+        hingeJoint1.connectedAnchor = anchor1;
+        Vector3 anchor2 = new Vector3(-1.0064f, 1.004f, -5.078898f);
+        hingeJoint2.connectedAnchor = anchor2;
+        */
+        UpdateSphereScale();
         UpdatePlatformScale();
     }
 
@@ -73,14 +113,19 @@ public class ScaleGameObjects : MonoBehaviour
 
     private void UpdatePlatformScale()
     {
-        // Update the scale of the platform based on the value of the platform slider
         float platformScale = platformSlider.value;
+        // Update the scale of the platform based on the value of the platform slider
+        
         exposureScene.transform.localScale = new Vector3(platformScale / 10, exposureScene.transform.localScale.y, platformScale / 10);
 
         sizeInputPlatform.text = platformScale.ToString();
 
+        //Vector3 newPosition = doors.transform.position - new Vector3(0, 0, platformScale*0.2f);
+        //doors.transform.position = newPosition;
         //exp_pos = wait_pos - 5 - exposureScene.transform.localScale * 5
         // door walls: pos_x +- (scale * size_x - size_x) / 2
+   
+        
     }
 
     private void UpdateSphereScale()
