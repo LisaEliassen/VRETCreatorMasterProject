9d145c0f7a0156d5e66ed211978775035e0227da
Princess Oly <olesya2011@hotmail.no>
2024-05-31 14:51:17 +0200

    Added copyright header

--- PhobiaFramework/Assets/Code/SoundManager.cs
+++ PhobiaFramework/Assets/Code/SoundManager.cs
@@ -1,3 +1,19 @@
+#region License
+// Copyright (C) 2024 Lisa Maria Eliassen & Olesya Pasichnyk
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the Commons Clause License version 1.0 with GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// Commons Clause License and GNU General Public License for more details.
+// 
+// You should have received a copy of the Commons Clause License and GNU General Public License
+// along with this program. If not, see <https://commonsclause.com/> and <https://www.gnu.org/licenses/>.
+#endregion
+
 using UnityEngine;
 using System;
 using SimpleFileBrowser;
